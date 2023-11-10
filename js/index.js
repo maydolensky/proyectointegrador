610a127de28900540e62cb32341cccad9f96249a
maydolensky <mdolensky@udesa.edu.ar>
2023-11-10 11:15:53 -0300

    index.js

--- js/index.js
+++ js/index.js
@@ -7,98 +7,102 @@ let API_URL_pelispopulares = baseURL + "movie/popular?" + APIKey;
 let API_URL_pelisvaloradas = baseURL + "movie/top_rated?" + APIKey;
 let API_URL_seriespopulares = baseURL + "tv/popular?" + APIKey;
 
-function getMovies(url, category, title) {
-    //la vamos a usar para obtener la información de la API, entenderla y usarla    
-    fetch(url)
-        .then(function(response) {
-            return response.json();
+fetch (API_URL_pelispopulares)
+    .then(function(response){
+        return response.json();
     })
-        .then(function(data) { 
-            console.log(data);
-            showMovies(data.results, category, title);           
-        });
-
-}
-
-function showMovies(data, category, title) {
-//nos va a servir para modularizar los resultados que queremos obtener de la API
-    // Agrega un título específico
-    let titleEl = document.createElement('h2');
-    titleEl.innerText = title;
-    main.appendChild(titleEl);
-
-    data.forEach(function(movie) {
-        //substrae la info que vamos a necesitar
-        let title = movie.title 
-        let poster_path = movie.poster_path;
-        let vote_average = movie.vote_average;
-        let release_date = movie.release_date 
-        let mediaType = movie.media_type; // Agregado para distinguir entre películas y series
-
-        let movieEl = document.createElement('article'); //crea un article
-        movieEl.classList.add(category); //le pone una clase
-
-        let linkEl = document.createElement('a'); //nos envía al detalle de series o pelis
-        if (mediaType === 'movie') {
-            linkEl.href = './peliculas_detalles.html';
-        } else if (mediaType === 'tv') {
-            linkEl.href = './series_detalles.html';
-        }          
-
-        let imgEl = document.createElement('img'); //permite ver la img
-        imgEl.src = poster_path ? IMG_URL + poster_path : "http://via.placeholder.com/1080x1580";
-        imgEl.alt = title;
-
-        linkEl.appendChild(imgEl);
-
-        let infoPeliEl = document.createElement('div');//crea un div y su clave
-        infoPeliEl.classList.add('infopeli');
-
-        let nameLinkEl = document.createElement('a');
-        if (mediaType === 'movie') {
-            nameLinkEl.href = './peliculas_detalles.html';
-        } else if (mediaType === 'tv') {
-            nameLinkEl.href = './series_detalles.html';
+    .then(function(data){
+      
+        let arrayMovies= data.results;
+        console.log(arrayMovies); 
+        
+        let listaMovies= document.querySelector("#main");
+        let peliculas=""
+        for (let i = 0; i < 20; i++) {
+            peliculas += `
+                <article class="populares">
+                    <div class="infopelis"> 
+                        <a href="peliculas_detalles.html?id=${arrayMovies[i].id}">
+                            <img src="${IMG_URL}${arrayMovies[i].poster_path}" alt="${arrayMovies[i].title}">
+                        </a>
+                        <h3>
+                            <a href="peliculas_detalles.html?id=${arrayMovies[i].id}">${arrayMovies[i].title}</a>
+                        </h3>
+                        <p class="release-date">
+                            <a href="peliculas_detalles.html?id=${arrayMovies[i].id}">${arrayMovies[i].release_date}</a>
+                        </p>
+                    </div>
+                </article>`;
         }
-        nameLinkEl.innerText = title;        
+        listaMovies.innerHTML= peliculas
+        
+    })
+    .catch(function(error){
+        console.log( "Error: " + error);
+    })
 
-        let releaseLinkEl = document.createElement('a');
-        if (mediaType === 'movie') {
-            releaseLinkEl.href = './peliculas_detalles.html';
-        } else if (mediaType === 'tv') {
-            releaseLinkEl.href = './series_detalles.html';
+    fetch (API_URL_pelisvaloradas)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+      
+        let arrayMovies= data.results;
+        console.log(arrayMovies); 
+        
+        let listaMovies= document.querySelector("#val");
+        let peliculas=""
+        for (let i = 0; i < 20; i++) {
+            peliculas += `
+                <article class="populares">
+                    <div class="infopelis"> 
+                        <a href="peliculas_detalle.html?id=${arrayMovies[i].id}">
+                            <img src="${IMG_URL}${arrayMovies[i].poster_path}" alt="${arrayMovies[i].title}">
+                        </a>
+                        <h3>
+                            <a href="peliculas_detalle.html?id=${arrayMovies[i].id}">${arrayMovies[i].title}</a>
+                        </h3>
+                        <p class="release-date">
+                            <a href="peliculas_detalles.html?id=${arrayMovies[i].id}">${arrayMovies[i].release_date}</a>
+                        </p>
+                    </div>
+                </article>`;
         }
-        releaseLinkEl.innerText = 'Estreno: ' + release_date;
+        listaMovies.innerHTML= peliculas
+        
+    })
+    .catch(function(error){
+        console.log( "Error: " + error);
+    })
 
-        let heartIconEl = document.createElement('i');
-        heartIconEl.classList.add('fa-solid', 'fa-heart');
-        heartIconEl.style.color = '#ff0000';
+    fetch(API_URL_seriespopulares)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        let arraySeries = data.results;
+        console.log(arraySeries);
 
-        infoPeliEl.appendChild(nameLinkEl);
-        infoPeliEl.appendChild(document.createElement('br'));
-        infoPeliEl.appendChild(releaseLinkEl);
-        infoPeliEl.appendChild(heartIconEl);
-
-        movieEl.appendChild(linkEl);
-        movieEl.appendChild(infoPeliEl);
-
-
-
-        main.appendChild(movieEl);
+        let listaSeries = document.querySelector("#pop");
+        let series = "";
+        for (let i = 0; i < 20; i++) {
+            series += `
+                <article class="populares">
+                    <div class="infopelis"> 
+                        <a href="series_detalles.html?id=${arraySeries[i].id}">
+                            <img src="${IMG_URL}${arraySeries[i].poster_path}" alt="${arraySeries[i].title}">
+                        </a>
+                        <h3>
+                            <a href="series_detalles.html?id=${arraySeries[i].id}">${arraySeries[i].title}</a>
+                        </h3>
+                        <p class="release-date">
+                            <a href="series_detalles.html?id=${arraySeries[i].id}">${arraySeries[i].release_date}</a>
+                        </p>
+                    </div>
+                </article>`;
+        }
+        listaSeries.innerHTML = series;
+    })
+    .catch(function(error) {
+        console.log("Error: " + error);
     });
-    };
-
-//llamo a las funciones 
-getMovies(API_URL_pelispopulares, 'populares', 'Películas Populares')
-getMovies(API_URL_pelisvaloradas, 'valoradas', 'Películas Valoradas');
-getMovies(API_URL_seriespopulares, 'populares', 'Series Populares');
-// fetch("https://api.themoviedb.org/3/tv/popular?api_key=7bd62b07c70beb54f8320746a7049a45")
-//     .then(function (res) {
-//         return res.json()
-//     })
-//     .then(function (data) {
-//         console.log(data)
-//     })
-//     .catch(function (err) {
-//         console.log(err)
-//     })
