7913f05127a0bd90602dfaeb4890c92ad5090a4c
VickyForginieri <victoriaforginieri@Victorias-MacBook-Pro.local>
2023-11-13 10:47:21 -0300

    cambios genero

--- js/genero.js
+++ js/genero.js
@@ -12,9 +12,9 @@ fetch (url_genres)
         let listaGenres= document.querySelector(".contenedor_genres");
         let genres=""
         for(let i=1; i< 5; i++){
-            genres += `<ul> <li> <h3> <a class="genre" href="./detail-genres.html?id=${arrayGenres[i].id}">${arrayGenres[i].name}</a> </h3> </li> </ul>
+            genres += `<ul> <li> <h3> <a class="genre" href="./genero_detalles.html?id=${arrayGenres[i].id}">${arrayGenres[i].name}</a> </h3> </li> </ul>
             
-            <a class="genre" href="./detail-genres.html?id=${arrayGenres[i].id}"><img src="${arrayGenres[i].picture}" alt="${arrayGenres[i].name}"></a>`
+            <a class="genre" href="./genero_detalles.html?id=${arrayGenres[i].id}"><img src="${arrayGenres[i].picture}" alt="${arrayGenres[i].name}"></a>`
     }
         listaGenres.innerHTML= genres 
     })
