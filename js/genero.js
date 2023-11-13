260394bdd244a0c03f9382da69b568371e4f3468
VickyForginieri <victoriaforginieri@Victorias-MacBook-Pro.local>
2023-11-13 13:24:19 -0300

    sigue sin funcionar correctamente el genero

--- js/genero.js
+++ js/genero.js
@@ -1,4 +1,4 @@
-const url_genres = "https://api.themoviedb.org/3/genre/movie/list"
+const url_genres = "https://api.themoviedb.org/3/genre/movie/list?api_key=7bd62b07c70beb54f8320746a7049a45";
  
 fetch (url_genres)
     .then(function(response){
@@ -6,17 +6,16 @@ fetch (url_genres)
     })
     .then(function(data){
       
-        let arrayGenres= data.data;
+        let arrayGenres= data.genres;
         console.log(arrayGenres); 
         
         let listaGenres= document.querySelector(".contenedor_genres");
-        let genres=""
-        for(let i=1; i< 5; i++){
-            genres += `<ul> <li> <h3> <a class="genre" href="./genero_detalles.html?id=${arrayGenres[i].id}">${arrayGenres[i].name}</a> </h3> </li> </ul>
+        let genres="";
+        for(let i=0; i< 5; i++){
+            genres += `<ul> <li> <h3> <a class="genre" href="./genero_detalles.html?id=${arrayGenres[i].id}">${arrayGenres[i].name}</a> </h3> </li> </ul>` ;
             
-            <a class="genre" href="./genero_detalles.html?id=${arrayGenres[i].id}"><img src="${arrayGenres[i].picture}" alt="${arrayGenres[i].name}"></a>`
     }
-        listaGenres.innerHTML= genres 
+        listaGenres.innerHTML= genres;
     })
     .catch(function(error){
         console.log("Error: " + error);
