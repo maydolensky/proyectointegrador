11c7146df936c91e7a00b3a9afc7a2a3fdaa9c07
VickyForginieri <victoriaforginieri@Victorias-MacBook-Pro.local>
2023-11-15 14:03:26 -0300

    correcion script

--- js/peliculas_detalles.js
+++ js/peliculas_detalles.js
@@ -14,21 +14,21 @@ fetch (URL_detallepeli)
         <div class="contenedor_peliculas"> 
 
             <section class="imagenes_sola">
-                <a href="URL_detallepeli?id=${listaMovies[i].id}"><h2>Genero</h2></a>
+                <a href="URL_detallepeli?id=${listaMovies.id}"><h2>Genero</h2></a>
                     <article class="fotogrande">
-                        <img src="${IMG_URL}${listaMovies[i].poster_path}" alt="${listaMovies[i].title}">
+                        <img src="${IMG_URL}${listaMovies.poster_path}" alt="${listaMovies.title}">
                         <div class="infoseriegrande">
                             <a href=""><h2>Titulo</h2></a> <br>
-                            <a href="URL_detallepeli?id=${listaMovies[i].id}">${listaMovies[i].release_date}">Estreno</a>
+                            <a href="URL_detallepeli?id=${listaMovies.id}">${listaMovies.release_date}">Estreno</a>
                             <i class="fa-solid fa-heart" style="color: #ffffff;"></i>
                             <p class="data_peli">
-                                <a href="URL_detallepeli?id=${listaMovies[i].id}">${listaMovies[i].overview}</a>
+                                <a href="URL_detallepeli?id=${listaMovies.id}">${listaMovies.overview}</a>
                             </p>
                             <p class="Duración">
                                 //NO ENCONTRE EL DE DURACION
                             </p>
                             <p class="Rating">
-                                <a href="URL_detallepeli?id=${listaMovies[i].id}">${listaMovies[i].vote_average}</a>
+                                <a href="URL_detallepeli?id=${listaMovies.id}">${listaMovies.vote_average}</a>
                             </p>
                         </div>
                     </article>
