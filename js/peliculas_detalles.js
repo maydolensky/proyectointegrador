b60e06f1ebc3973f8172ed5d6800671550691112
VickyForginieri <victoriaforginieri@Victorias-MacBook-Pro.local>
2023-11-16 14:34:03 -0300

    correciones peliculas

--- js/peliculas_detalles.js
+++ js/peliculas_detalles.js
@@ -1,4 +1,4 @@
-let URL_detallepeli = `https://api.themoviedb.org/3/movie/${id_pelicula}?api_key=api_key=7bd62b07c70beb54f8320746a7049a45}`
+let URL_detallepeli = `https://api.themoviedb.org/3/movie/${id_pelicula}?api_key=api_key=7bd62b07c70beb54f8320746a7049a45`
 
 fetch (URL_detallepeli)
     .then(function(response){
@@ -6,7 +6,7 @@ fetch (URL_detallepeli)
     })
     .then(function(data){
       
-        let arrayDetallesPeli= data.results;
+        let arrayDetallesPeli= data;
         console.log(arrayDetallesPeli); 
         let listaMovies= document.querySelector(".contenedor_peliculas");
         let peliculas=""
