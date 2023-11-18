fdd675798a9034d616fa2fb71bac0a07365189fe
maydolensky <mdolensky@udesa.edu.ar>
2023-11-17 21:55:25 -0300

    agrego genero de series + logo de TMDB en footer

--- js/genero.js
+++ js/genero.js
@@ -1,8 +1,9 @@
 let url_genres = "https://api.themoviedb.org/3/genre/movie/list?api_key=7bd62b07c70beb54f8320746a7049a45";
+let url_genres_series= "https://api.themoviedb.org/3/genre/tv/list?api_key=7bd62b07c70beb54f8320746a7049a45";
 let queryString = location.search
 let queryStringObj = new URLSearchParams(queryString);
-let busqueda = queryStringObj.get("busqueda"); //agarra el input //guarda el valor de mi clave
-let IMG_URL = "https://image.tmdb.org/t/p/w500";
+let busqueda = queryStringObj.get("busqueda"); 
+let IMG_URL = "https://image.tmdb.org/t/p/w500"; //NO QUITAAAR
 
 
 fetch(url_genres)
@@ -10,7 +11,7 @@ fetch(url_genres)
     return response.json();
   })
   .then(function(data) {
-    console.log(data); // Log the entire data structure to the console
+    console.log(data); 
 
     let arrayGenres = data.genres;
     console.log(arrayGenres);
@@ -31,6 +32,38 @@ fetch(url_genres)
     }
 
     listaGenres.innerHTML = genres;
+  })
+  .catch(function(error) {
+    console.log("Error: " + error);
+  });
+
+
+fetch(url_genres_series)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data); 
+
+    let arrayGenresS = data.genres;
+    console.log(arrayGenresS);
+
+    let listaGenresS = document.querySelector(".contengo_generos_series");
+    let genres = "";
+
+    for (let i = 0; i < 5; i++) {
+      genres += `<article class="bloque_de_generos"> 
+                    <ul class="contenedor_genres"> 
+                        <li> 
+                            <h2> 
+                                <a class="genero" href="./genero_detalles.html?id=${arrayGenresS[i].id}">${arrayGenresS[i].name}</a> 
+                            </h2> 
+                        </li> 
+                    </ul>
+                </article>`;
+    }
+
+    listaGenresS.innerHTML = genres;
   })
   .catch(function(error) {
     console.log("Error: " + error);
